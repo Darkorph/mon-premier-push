12b016a2b5f8ab6affc2f0e762eb933421d49d0a
Darkorph <y.crochon@gmail.com>
2019-01-17 10:24:10 +0100

    premierCommit

---
+++ contact.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>index</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+</head>
+<body>
+    <p>Ceci est la page contact</p>
+</body>
+</html>
